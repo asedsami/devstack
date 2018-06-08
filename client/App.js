8c464848419bb5178799e150942285333362d4d4
asedsami <asedsami@gmail.com>
2018-06-08 08:46:34 +0430

    tidy

--- client/App.js
+++ client/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
 
 const Home = ()=> (<h1>Home</h1>);
-const Roster = ()=> (<h1>Roster</h1>);
-const NotFound = ()=> (<h1>NotFound</h1>);
 
 const App = ()=> (
 	<main>
-		<h1>h1 in App.js and index.html</h1>
-		<nav>
-			<ul>
-				<li><Link to='/'>Home</Link></li>	
-				<li><Link to='/roster'>Roster</Link></li>	
-				<li><Link to='/hi'>hi</Link></li>	
-			</ul>	
-		</nav>
-		<Switch>
-			<Route exact path='/' component={Home}/>
-			<Route path='/roster' component={Roster}/>
-			<Route path='*' component={NotFound}/>
-		</Switch>
+		<Home />
 	</main>
 );
 
